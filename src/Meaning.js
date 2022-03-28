c1ce47096fd5cd1075dcc800b31b9e48d38bfcff
bridgetbidigare <90158212+bridgetbidigare@users.noreply.github.com>
2022-03-28 18:38:09 -0500

    Added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
         <div>
             <h3>{props.meaning.partOfSpeech}</h3>
-            {props.meaning.definitions.map(function(definiton, index) {
+            {props.meaning.definitions.map(function(definition, index) {
                 return (
                 <div key={index}>
-                    {definiton.definition}
+                    {definition.definition}
                     <br />
-                    <em>{definiton.expample}</em>
+                    <em>{definition.example}</em>
+                    <br />
+                    <Synonyms synonyms={definition.synonyms} />
+                    <Synonyms synonyms={props.meaning.synonyms} />
                 </div>
                 );
             })}
